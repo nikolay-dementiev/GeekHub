bfb04c70337df1356a2686636f4f7076b215a37f
Dementiev Nikolay <dementievnikolay@gmail.com>
2016-10-26 20:10:34 +0300

    #1 ready

--- 26-10-2010-#21/Obj-C-LineAndArray/Obj-C-LineAndArray/main.m
+++ 26-10-2010-#21/Obj-C-LineAndArray/Obj-C-LineAndArray/main.m
@@ -8,12 +8,71 @@
 
 #import <Foundation/Foundation.h>
 
+@interface SampleLineClass: NSObject
+
+//- (NSNumber *)multiplyA:(NSNumber *)a withB:(NSNumber *)b;
+//1
+- (void) testSomeLine;
+@end
+
+@implementation SampleLineClass
+
+//MARK: - Lines
+//MARK: #1
+- (void) testSomeLine {
+
+		NSString *lineForTest = @"Дана строка, содержащая текст. Найти длину самого короткого слова и самого длинного слова.";
+
+		//remove special symbols
+		NSArray *specSymb = [NSArray arrayWithObjects:@".", @";", @":", @",", nil];
+		int idx = 0;
+		while (idx < [specSymb count]) {
+				lineForTest = [lineForTest stringByReplacingOccurrencesOfString: specSymb[idx] withString:@""];
+				idx++;
+		}
+
+		//1 Break the line by words
+		NSMutableArray *parts = [NSMutableArray arrayWithArray:
+																						[lineForTest componentsSeparatedByCharactersInSet:
+																						[NSCharacterSet  whitespaceCharacterSet]]];
+
+		[parts removeObjectIdenticalTo:@""];
+
+		// calculate
+		int ind = 0;
+		NSMutableString *res;
+		int maxCount = 0;
+		int minCount = 0;
+		BOOL firstIn = TRUE;
+		for (ind; ind < parts.count; ind++) {
+				res = [parts objectAtIndex: ind];
+
+				int countRes = (int)[res length];
+				maxCount = countRes > maxCount ? countRes : maxCount;
+
+				if (firstIn && countRes > 0) {
+						firstIn = FALSE;
+						minCount = countRes;
+				} else {
+						minCount = countRes < minCount ? countRes : minCount;
+				}
+
+		}
+
+		NSLog(@"max. length = '%d'; min. length = '%d'.", maxCount, minCount);
+}
+
+
+@end
+
 int main(int argc, const char * argv[]) {
 	@autoreleasepool {
 	    // insert code here...
-	    NSLog(@"Hello, World!");
+			SampleLineClass *sampleClass = [SampleLineClass new];
+
+			//#1
+			[sampleClass testSomeLine];
 	}
 
-		
     return 0;
 }
