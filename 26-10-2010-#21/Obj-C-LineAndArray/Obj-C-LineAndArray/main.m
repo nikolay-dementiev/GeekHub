c1aae28a2e2acffd3eef4051ac185ac4cb11d8c9
Dementiev Nikolay <dementievnikolay@gmail.com>
2016-10-27 15:06:17 +0300

    #4 ready. FindTheAmountOfAvailableNumbersInTheText

--- 26-10-2010-#21/Obj-C-LineAndArray/Obj-C-LineAndArray/main.m
+++ 26-10-2010-#21/Obj-C-LineAndArray/Obj-C-LineAndArray/main.m
@@ -15,6 +15,7 @@
 - (void) CalculateTheMinimumAndMaximumNumberOfCharactersPerLine;
 - (void) CalculateHowManyDifferentSymbolsOccursOnLine;
 - (void) FindTheWordThatBeginsAndEndsWithTheSameLetter;
+- (void) FindTheAmountOfAvailableNumbersInTheText;
 
 + (NSString *) RemoveSpecialSymbolsInTheLine: (NSString *)line;
 @end
@@ -47,18 +48,17 @@
 
 		//1 Break the line by words
 		NSMutableArray *parts = [NSMutableArray arrayWithArray:
-																						[lineForTest componentsSeparatedByCharactersInSet:
-																						[NSCharacterSet  whitespaceCharacterSet]]];
+														 [lineForTest componentsSeparatedByCharactersInSet:
+															[NSCharacterSet  whitespaceCharacterSet]]];
 
-		[parts removeObjectIdenticalTo:@""];
+		[parts removeObject:@""];
 
 		// calculate
-		int ind = 0;
 		NSMutableString *res;
 		int maxCount = 0;
 		int minCount = 0;
 		BOOL firstIn = TRUE;
-		for (ind; ind < parts.count; ind++) {
+		for (int ind = 0; ind < parts.count; ind++) {
 				res = [parts objectAtIndex: ind];
 
 				int countRes = (int)[res length];
@@ -124,16 +124,15 @@
 														 [lineForTest componentsSeparatedByCharactersInSet:
 															[NSCharacterSet  whitespaceCharacterSet]]];
 
-		[parts removeObjectIdenticalTo:@""];
+		[parts removeObject:@""];
 
-		int ind = 0;
 		NSString *res;
 		NSString *firstLetter;
 		NSString *lastLetter;
 
 		NSMutableArray* theSameLetterWords = [NSMutableArray new];
 
-		for (ind; ind < parts.count; ind++) {
+		for (int ind = 0; ind < parts.count; ind++) {
 				res = [parts objectAtIndex: ind];
 
 				int countRes = (int)[res length];
@@ -161,27 +160,95 @@
 										 ([strOutPut length] > 0 ? @", ": @" "),
 										 [element description]];
 		}
+
 		NSLog(@ "Words that begins and ends with the same letter:  %@", strOutPut);
+}
+
+//MARK: #4
+
+- (void) FindTheAmountOfAvailableNumbersInTheText {
+		NSLog(@ "#4 - FindTheAmountOfAvailableNumbersInTheText");
+
+		NSString *lineForTest = @"Дан1 текст23. 32 Найти сумму имею456щихся в0 нем ц5ифр.";
+		NSString *dotSymbol = @".";
+
+		NSMutableString *strippedString = [NSMutableString new];
+		for (int i=0; i<[lineForTest length]; i++) {
+				unichar cType = [lineForTest characterAtIndex:i];
+				unichar chDot = [dotSymbol characterAtIndex:0];
+				if (isdigit(cType) || cType == chDot) {
+						[strippedString appendFormat:@"%c",[lineForTest characterAtIndex:i]];
+				} else {
+						[strippedString appendFormat:@"%@",@" "];
+				}
+		}
+
+		NSMutableArray *parts = [NSMutableArray arrayWithArray:
+														 [strippedString componentsSeparatedByCharactersInSet:
+															[NSCharacterSet  whitespaceCharacterSet]]];
+
+		[parts removeObject: @""];
+		// let's delete self-staying dot symbol
+		[parts removeObject: dotSymbol];
+
+		float resultOfCalc = 0;
+
+		NSString* strOuput = @"";
+		int step = 0;
+		while (step < [parts count]) {
+				NSString* strTemp = [parts objectAtIndex: step];
+
+				if ([strTemp containsString: dotSymbol]) {
+						if ([strTemp hasPrefix: dotSymbol] || [strTemp hasSuffix: dotSymbol]) {
+								//smth went wrong! Let's say it..
+								NSLog(@"Something is wrong in expression '%@'! Symbol '%@' cann't stay at the beginning or at the end of expression. Current step was passed.", strTemp, dotSymbol);
+
+								step++;
+								continue;
+						}
+				}
+
+				strOuput = [strOuput stringByAppendingFormat: @"%@ + ",strTemp];
+				resultOfCalc = resultOfCalc +  [strTemp floatValue];
+
+				step++;
+		}
+
+		//delete excess tail in expression
+		NSRange excessTail = [strOuput rangeOfString:@" + " options:NSBackwardsSearch];
+		if(excessTail.location != NSNotFound) {
+				strOuput = [strOuput stringByReplacingCharactersInRange:excessTail withString: @""];
+		}
+
+		//Output the results of calculations
+		NSLog(@ "Resulst of expresssion '%@' = '%.02f'", strOuput, resultOfCalc);
 
 }
 
+
 @end
 
+
+
+//MARK: - init
+
 int main(int argc, const char * argv[]) {
-	@autoreleasepool {
-	    // insert code here...
-			SampleLineClass *sampleClass = [SampleLineClass new];
+		@autoreleasepool {
+				// insert code here...
+				SampleLineClass *sampleClass = [SampleLineClass new];
 
-			//#1
-			[sampleClass CalculateTheMinimumAndMaximumNumberOfCharactersPerLine];
+				//#1
+				[sampleClass CalculateTheMinimumAndMaximumNumberOfCharactersPerLine];
 
-			//#2
-			[sampleClass CalculateHowManyDifferentSymbolsOccursOnLine];
+				//#2
+				[sampleClass CalculateHowManyDifferentSymbolsOccursOnLine];
 
-			//#3
-			[sampleClass FindTheWordThatBeginsAndEndsWithTheSameLetter];
-
-	}
-
-    return 0;
+				//#3
+				[sampleClass FindTheWordThatBeginsAndEndsWithTheSameLetter];
+				
+				//#4
+				[sampleClass FindTheAmountOfAvailableNumbersInTheText];
+		}
+		
+		return 0;
 }
