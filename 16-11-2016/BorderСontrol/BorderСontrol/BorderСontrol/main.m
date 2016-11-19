43baf3e8ec97dd7a09b4d539489392f959ff58f0
Dementiev Nikolay <dementievnikolay@gmail.com>
2016-11-19 16:10:08 +0200

    #4 - Border control - initial commit

---
+++ 16-11-2016/BorderСontrol/BorderСontrol/BorderСontrol/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  BorderСontrol
+//
+//  Created by Nikolay Dementiev on 19.11.16.
+//  Copyright © 2016 mc373. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
