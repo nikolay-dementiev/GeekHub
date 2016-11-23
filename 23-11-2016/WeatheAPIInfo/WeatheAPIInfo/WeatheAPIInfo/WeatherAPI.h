5fb88ff45c295beae43cf9f28876160486e18e50
Dementiev Nikolay <dementievnikolay@gmail.com>
2016-11-23 19:41:44 +0200

    weather API info

---
+++ 23-11-2016/WeatheAPIInfo/WeatheAPIInfo/WeatheAPIInfo/WeatherAPI.h
@@ -0,0 +1,16 @@
+//
+//  WeatherAPI.h
+//  WeatheAPIInfo
+//
+//  Created by Nikolay Dementiev on 23.11.16.
+//  Copyright © 2016 mc373. All rights reserved.
+//
+
+#ifndef WeatherAPI_h
+#define WeatherAPI_h
+
+@interface WeatherAPI : NSObject
+
+@end
+
+#endif /* WeatherAPI_h */
---
+++ 23-11-2016/WeatheAPIInfo/WeatheAPIInfo/WeatheAPIInfo/WeatherAPI.m
@@ -0,0 +1,15 @@
+//
+//  WeatherAPI.m
+//  WeatheAPIInfo
+//
+//  Created by Nikolay Dementiev on 23.11.16.
+//  Copyright © 2016 mc373. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "WeatherAPI.h"
+
+@implementation WeatherAPI
+
+
+@end
